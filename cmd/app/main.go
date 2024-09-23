2d6f3ae80d4ca39c68017ddf17110a0cd91e892a
lakeside763 <lakeside763@gmail.com>
2024-09-23 10:09:04 +0100

    chore: loading configuration with init and implement shutdown gracefully method

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/julienschmidt/httprouter"
@@ -14,15 +20,16 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func initEnv() {
+func init() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatalf("Error loading .evn file: %v", err)
+	} else {
+		log.Info("Loading configuration")
 	}
 }
 
 func main() {
 	// Initialize dotenv
-	initEnv()
 	config := config.LoadConfig()
 
 	// Initialize repositories
@@ -33,8 +40,16 @@ func main() {
 	// Initialize Github API Client
 	githubAPIClient := clients.NewGithubAPIClient(config.GithubAPIBaseURL, config.GithubToken)
 
+
+	// Create a context for cancellation and graceful shutdown
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Create a WaitGroup to wait for goroutines to finish
+	var wg sync.WaitGroup
+
 	// Initialize Repometadata service
-	repoMetadataService := services.NewRepoMetadataService(githubAPIClient, redisClient, dataRepo.Repometadata,)
+	repoMetadataService := services.NewRepoMetadataService(githubAPIClient, redisClient, dataRepo.Repometadata)
 	_, err := repoMetadataService.FetchRepoMetadata(config.RepoFullName)
 	if err != nil {
 		log.Fatalf("Failed to fetch repo metadata: %v", err)
@@ -42,13 +57,60 @@ func main() {
 
 	// Initialize Commit Monitoring Service
 	monitoringService := services.NewMonitoringService(*githubAPIClient, redisClient, dataRepo.Commit)
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		monitoringService.StartMonitoring(config.MonitoringInterval, config.RepoFullName, config.FetchLimit)
 	}()
 
 	router := httprouter.New()
 	routes.SetupRoutes(router, dataRepo)
+
+	server := &http.Server{
+		Addr: ":5200",
+		Handler: router,
+	}
+
+	// Run the HTTP server in a goroutine
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		log.Info("Starting server on :5200")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server failed to serve: %v", err)
+		}
+	}()
 	
-	log.Info("Starting server on :5200")
-	log.Fatal(http.ListenAndServe(":5200", router))
+	// log.Info("Starting server on :5200")
+	// log.Fatal(http.ListenAndServe(":5200", router))
+	
+	// Handle graceful shutdown
+	waitForShutdown(ctx, cancel, server, &wg)
+}
+
+func waitForShutdown(ctx context.Context, cancel context.CancelFunc, server *http.Server, wg *sync.WaitGroup) {
+	// Capture termination signals
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+	// wait for a termination signal
+	<-sigChan
+	log.Info("Shutting down gracefully")
+
+	// Cancel the context to signal goroutines to stop
+	cancel()
+
+	// Gracefully shut down the HTTP server with a timeout
+	ctxShutdown, cancelShutdown := context.WithTimeout(ctx, 5*time.Second)
+	defer cancelShutdown()
+
+	if err := server.Shutdown(ctxShutdown); err != nil {
+		log.Errorf("Server forced to shutdown: %v", err)
+	} else {
+		log.Info("HTTP server stopped gracefully")
+	}
+
+	// Wait for all goroutines to finish
+	wg.Wait()
+	log.Info("All services stopped")
 }
